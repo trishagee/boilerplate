e4ef9d427cd5449975202a66a6ca7f71656d6589
Trisha Gee <trisha.gee@gmail.com>
2018-02-27 14:50:27 +0100

    Need a test for the nulls

---
+++ src/test/java/com/mechanitis/demo/boilerplate/example3/NullsTest.java
@@ -0,0 +1,10 @@
+package com.mechanitis.demo.boilerplate.example3;
+
+import org.junit.jupiter.api.Test;
+
+class NullsTest {
+    @Test
+    void testForNulls() {
+        NullHandling.validateCustomerWithNulls
+    }
+}
